d2d080ae143b23b6d11735903f2f503d3fd61f03
Bluemoose76 <soren.pat2@gmail.com>
2023-08-29 18:55:16 -0500

    may or may not fixed spawning issue with ore

---
+++ Common/Systems/GenPasses/SolOreGenPass.cs
@@ -0,0 +1,31 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.IO;
+using Terraria.WorldBuilding;
+using Taiterraria.Content.Tiles;
+using Terraria.ID;
+
+namespace Taiterraria.Common.Systems.GenPasses
+{
+    internal class TutorialOreGenPass : GenPass
+    {
+        public TutorialOreGenPass(string name, float weight) : base(name, weight) { }
+
+        protected override void ApplyPass(GenerationProgress progress, GameConfiguration configuration)
+        {
+            progress.Message = "Spawning Taite MOD Ores";
+
+            // SOL ore
+            int maxToSpawn = (int)(Main.maxTilesX * Main.maxTilesY * 6E-05);
+            for(int i = 0; i < maxToSpawn; i++)
+            {
+                int x = WorldGen.genRand.Next(100, Main.maxTilesX - 100);
+                int y = WorldGen.genRand.Next((int)GenVars.worldSurface, Main.maxTilesY - 300);
+
+                WorldGen.TileRunner(x, y, WorldGen.genRand.Next(3, 6), WorldGen.genRand.Next(2, 5), ModContent.TileType<SolOre>());
+            }
+
+           
+        }
+    }
+}
---
+++ Common/Systems/WorldSystem.cs
@@ -0,0 +1,19 @@
+﻿using Terraria.ModLoader;
+using Terraria.WorldBuilding;
+using System.Collections.Generic;
+using Taiterraria.Common.Systems.GenPasses;
+
+namespace Taiterraria.Common.Systems
+{
+    internal class WorldSystem : ModSystem
+    {
+        public override void ModifyWorldGenTasks(List<GenPass> tasks, ref double totalWeight)
+        {
+            int shiniesIndex = tasks.FindIndex(t => t.Name.Equals("Shinies"));
+            if(shiniesIndex != -1)
+            {
+              //  tasks.Insert(shiniesIndex + 1, new Taiterraria.Common.Systems.GenPasses.SolOreGenPass("Sol Ore Pass", 320f));
+            }
+        }
+    }
+}
