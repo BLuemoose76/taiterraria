18b1b6e9f76dac2cf864e2045d2f0a8f18391bba
Bluemoose76 <soren.pat2@gmail.com>
2023-08-29 11:28:59 -0500

    first test item done

--- Content/Items/Weapons/silverslicer.cs
+++ Content/Items/Weapons/silverslicer.cs
@@ -1,43 +1,50 @@
-using Microsoft.Xna.Framework;
-using Terraria;
-using Terraria.ID;
+﻿using Terraria;
 using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
 
-namespace Terraria.Content.Items.Weapons
+namespace taiterraria.Content.Items.Weapons
 {
-	public class Silverslicer : ModItem
-	{
-		public override void SetDefaults() {
-			Item.width = 40; // The item texture's width.
-			Item.height = 40; // The item texture's height.
+    internal class silverslicer : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
 
-			Item.useStyle = ItemUseStyleID.Swing; // The useStyle of the Item.
-			Item.useTime = 20; // The time span of using the weapon. Remember in terraria, 60 frames is a second.
-			Item.useAnimation = 20; // The time span of the using animation of the weapon, suggest setting it the same as useTime.
-			Item.autoReuse = true; // Whether the weapon can be used more than once automatically by holding the use button.
+        public override void SetDefaults()
+        {
+            // Hitbox
+            Item.width = 70;
+            Item.height = 70;
 
-			Item.DamageType = DamageClass.Melee; // Whether your item is part of the melee class.
-			Item.damage = 20; // The damage your item deals.
-			Item.knockBack = 3; // The force of knockback of the weapon. Maximum is 20
-			Item.crit = 10; // The critical strike chance the weapon has. The player, by default, has a 4% critical strike chance.
+            // Use and Animation Style
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTime = 20;
+            Item.useAnimation = 20;
+            Item.autoReuse = true;
+            
+            // Damage Values
+            Item.DamageType = DamageClass.Melee;
+            Item.damage = 20;
+            Item.knockBack = 3;
+            Item.crit = 10;
 
-			Item.value = Item.buyPrice(gold: 1); // The value of the weapon in copper coins.
-			Item.rare = 2; // Give this item our custom rarity.
-			Item.UseSound = SoundID.Item1; // The sound when the weapon is being used.
-		}
+            // Misc
+            Item.SetNameOverride("Silver Slicer");
+            Item.rare = ItemRarityID.Blue;
 
-	
+            // Sound
+            Item.UseSound = SoundID.Item1;
+        }
 
-
-
-		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
-		public override void AddRecipes()
+        public override void AddRecipes()
 		{
 			Recipe recipe = CreateRecipe();
 			recipe.AddIngredient(ItemID.SilverBar, 30);
-            recipe.AddIngredient(ItemID.Wood, 10)
-			recipe.AddTile(TileID.IronAnvil);
+            recipe.AddIngredient(ItemID.Wood, 10);
+			recipe.AddTile(TileID.Anvils);
 			recipe.Register();
 		}
-	}
+    }
 }
