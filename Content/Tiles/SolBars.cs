3015e1142c85c9a3bb3752977b4995f1753b9292
Bluemoose76 <soren.pat2@gmail.com>
2023-08-29 15:07:04 -0500

    cleaner

---
+++ Content/Tiles/SolBars.cs
@@ -0,0 +1,26 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terraria.DataStructures;
+using Terraria.ObjectData;
+using Terraria.Localization;
+
+namespace Taiterraria.Content.Tiles
+{
+    internal class SolBars : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileSolidTop[Type] = true;
+            Main.tileShine[Type] = 1100;
+            Main.tileFrameImportant[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style1x1);
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.LavaDeath = false;
+            TileObjectData.addTile(Type);
+
+            AddMapEntry(new Color(209, 204, 19), Language.GetText("MapObject.MetalBar"));
+        }
+    }}
