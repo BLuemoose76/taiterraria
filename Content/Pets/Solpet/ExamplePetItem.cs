5696bac37ff8aafc2223992e5de1522b7148161f
Bluemoose76 <soren.pat2@gmail.com>
2023-08-29 21:59:28 -0500

    bugfixing

---
+++ Content/Pets/Solpet/ExamplePetItem.cs
@@ -0,0 +1,39 @@
+﻿using Taiterraria.Content.Items;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+
+namespace Taiterraria.Content.Pets.Solpet
+{
+	public class ExamplePetItem : ModItem
+	{
+		public override void SetStaticDefaults() {
+			// Names and descriptions of all ExamplePetX classes are defined using .hjson files in the Localization folder
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults() {
+			Item.CloneDefaults(ItemID.ZephyrFish); // Copy the Defaults of the Zephyr Fish Item.
+
+			Item.shoot = ModContent.ProjectileType<ExamplePetProjectile>(); // "Shoot" your pet projectile.
+			Item.buffType = ModContent.BuffType<ExamplePetBuff>(); // Apply buff upon usage of the Item.
+		}
+
+		public override void UseStyle(Player player, Rectangle heldItemFrame) {
+			if (player.whoAmI == Main.myPlayer && player.itemTime == 0) {
+				player.AddBuff(Item.buffType, 3600);
+			}
+		}
+
+		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+		public override void AddRecipes() {
+			CreateRecipe()
+				.AddIngredient<Content.Items.Consumables.LifeCrystalMilk>(1)
+				.AddIngredient<Content.Items.Placeables.SolBar>(10)
+				.AddTile(TileID.Anvils)
+				.Register();
+		}
+	}
+}
